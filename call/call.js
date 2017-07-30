e804a2345d594e742d1a3636338f2ad1c50e7462
minhtung404 <30457418+minhtung404@users.noreply.github.com>
2017-07-30 10:14:22 +0700

    Update call.js

--- call/call.js
+++ call/call.js
@@ -28,7 +28,7 @@ function playStream(idVideoTag, stream) {
     video.play();
 }
 
-var confidStunTwili = {'iceServers': 
+var confidStunTwili = {'iceServers':
     [{"url":"stun:global.stun.twilio.com:3478?transport=udp"},
     {"url":"turn:global.turn.twilio.com:3478?transport=udp","username":"0c498ca5e28778a8df0f9c64e9e14725e6a716faa56302a3d171de49b7a65143","credential":"Fe1R00p/o7fesG/EPcgFL4h14yviMkozfHDtkV3uf90="},
     {"url":"turn:global.turn.twilio.com:3478?transport=tcp","username":"0c498ca5e28778a8df0f9c64e9e14725e6a716faa56302a3d171de49b7a65143","credential":"Fe1R00p/o7fesG/EPcgFL4h14yviMkozfHDtkV3uf90="},
@@ -46,21 +46,21 @@ var peer = new Peer({
 
 
 $(document).ready(function(){
-    
-    
+
+
     // Index.html
     var idNguoiChoi = getUrlParameter('idNguoiChoi');
     var tenNguoiChoi = getUrlParameter('tenNguoiChoi');
     var tenTeam = getUrlParameter('tenTeam');
-    
-    
+
+
     var socketId;
-    
+
     var objThongTinNguoiChoi={idNguoiChoi:idNguoiChoi,tenNguoiChoi:tenNguoiChoi,tenTeam:tenTeam};
-   
-    
-    setTimeout(function(){ 
-               
+
+
+    setTimeout(function(){
+
                 openStream().then(stream=>{
                         playStream('bgvid',stream)
                         // mo duoc camera moi gui yeu cau
@@ -68,39 +68,42 @@ $(document).ready(function(){
                                 if(idNguoiChoi!=""){
                                     socket.emit("mb-yeu-cau-mo-camera",objThongTinNguoiChoi);
                                 }
-                             }           
+                             }
               });
-            
+
     }, 2000);
-    
-    
-    
+
+
+
     //
     socket.on("server-gui-stt-camera-cho-nguoi-choi",function(data){
                 if(data.sttCamera=="1"){
                     var codePeer = data.codePeer;
                     console.log(codePeer);
                     openStream().then(stream=>{
-                        peer.call(codePeer,stream);    
+                        peer.call(codePeer,stream);
                      });
                     $(".clsAfterVideo").css({"color":"green"});
                     $(".clsAfterVideo").css({"margin-top": "80%"});
-                    $('.clsAfterVideo').html('<i class="fa fa-connectdevelop animated infinite flipOutY" aria-hidden="true"></i>  Trực tiếp...'); 
-                } 
+                    $(".clsAfterVideo").css({"font-weight": "bold"});
+                    $('.clsAfterVideo').html('<i class="fa fa-connectdevelop animated infinite flipOutY" aria-hidden="true"></i>  Trực tiếp...');
+                }
                 else{
                     $(".clsAfterVideo").css({"color":"red"});
+                    $(".clsAfterVideo").css({"font-weight": "bold"});
                     $('.clsAfterVideo').html('<i class="fa fa-connectdevelop animated infinite flipOutY" aria-hidden="true"></i>  Kết nối bị từ chối, bạn chưa được sử dụng tác vụ này!');
                 }
     });
-    
-    
-    
-    
+
+
+
+
     // Disconect
     peer.on('disconnected', function() {
         $(".clsAfterVideo").css({"color":"red"});
+        $(".clsAfterVideo").css({"font-weight": "bold"});
         $('.clsAfterVideo').html('<i class="fa fa-connectdevelop animated infinite flipOutY" aria-hidden="true"></i>  Đã dừng kết nối!');
     });
-    
-             
+
+
 });
