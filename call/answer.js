aecb0fca8327cb5e799e7f856f0a11b7838a0286
minhtung404 <30457418+minhtung404@users.noreply.github.com>
2017-07-31 14:03:41 +0700

    Update answer.js

--- call/answer.js
+++ call/answer.js
@@ -18,18 +18,19 @@ function getUrlParameter(sParam) {
 
 //
   //Ham xu ly record video
-var recordRTC;
+
         function successCallback(stream) {
           // RecordRTC usage goes here
-          
+        
           var options = {
             mimeType: 'video/webm', // or video/webm\;codecs=h264 or video/webm\;codecs=vp9
             audioBitsPerSecond: 128000,
             videoBitsPerSecond: 128000,
             bitsPerSecond: 128000 // if this line is provided, skip above two
           };
-          recordRTC = RecordRTC(stream, options);
+          var recordRTC = RecordRTC(stream, options);
           recordRTC.startRecording();
+
       }
 
 
