7e573dbbdb24f1d727281c5dcfe098e15f452dd6
minhtung404 <30457418+minhtung404@users.noreply.github.com>
2017-07-31 13:58:25 +0700

    Update answer.js

--- call/answer.js
+++ call/answer.js
@@ -21,7 +21,7 @@ function getUrlParameter(sParam) {
 var recordRTC;
         function successCallback(stream) {
           // RecordRTC usage goes here
-          var recordRTC = recordRTC;
+          
           var options = {
             mimeType: 'video/webm', // or video/webm\;codecs=h264 or video/webm\;codecs=vp9
             audioBitsPerSecond: 128000,
